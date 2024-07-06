37ae6b8164dcda22984bfb535134f8307ca7af5b
dixitdevarshi <167009227+dixitdevarshi@users.noreply.github.com>
2024-07-06 17:49:47 +0530

    Update serializers.py

--- project/specs/serializers.py
+++ project/specs/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import *
 
-# class PhoneNameSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = PhoneName
-#         fields = ['id', 'phone_name']
+class PhoneNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhoneName
+        fields = '__all__'
+        
+        
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+        
